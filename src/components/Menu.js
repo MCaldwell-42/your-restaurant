7abbc66c28b0918d3313a82eb56eb962c74aeb9c
Matthew Caldwell <MCaldwell4242@gmail.com>
2020-06-04 12:18:45 -0500

    menu up

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Menu1 from './restaurantPics/menu1.jpg';
+import Menu2 from './restaurantPics/menu2.jpg';
 
 class Menu extends React.Component {
   render() {
     return (
-        <div className="special col-3">
-        <div className="card">
-          {/* <img className="card-img-top" src={special.imageUrl} alt="picture of special" /> */}
-          <div className="card-body">
-            <h5 className="card-title">Pictures of Menus here</h5>
-          </div>
+
+        <div className="special col-12">
+        <div className="card menuPics">
+          <img className="img-responsive" src={Menu1} alt="page 1 menu" />
+        </div>
+
+        <div className="card menuPics">
+          <img className="img-responsive" src={Menu2} alt="page 2 menu" />
         </div>
         </div>
     );
